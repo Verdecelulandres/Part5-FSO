5b9103b971725dac02650de39e7816b94b05bf81
Andres Laverde <andres.laverde@protonmail.com>
2026-08-18 21:19:10 -0700

    update: make form display block

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,22 +1,27 @@
-const LoginForm = ({ handleSubmit, handleChange, username, password }) => {
+const LoginForm = ({ handleLogin, handleUsername, handlePassword, username, password }) => {
   return (
     <>
       <h1>Login to application</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          username
-          <input 
-            onChange={handleChange}
-            value={username}
-          />
-        </label>
-        <label>
-          password
-          <input 
-            onChange={handleChange}
-            value={password}
-          />
-        </label>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              onChange={handleUsername}
+              value={username}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              onChange={handlePassword}
+              value={password}
+            />
+          </label>
+        </div>
         <button type="submit">login</button>
       </form>
     </>

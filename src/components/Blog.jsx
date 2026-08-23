1f023a90318374f524dfece23b3a578643253ca9
Andres Laverde <andres.laverde@protonmail.com>
2026-08-22 22:56:20 -0700

    update: add show full blog button

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div className="blog">
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [viewFull, setViewFull] = useState(false);
+
+  const toggleFullView = () => {
+    setViewFull(!viewFull);
+  }
+
+  return (
+    <div className="blog">
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleFullView}>
+          {viewFull ? 'hide' : 'view'}
+        </button>
+      </div>
+      {viewFull &&
+        <div>
+          <div>{blog.url}</div>
+          <div>
+           likes {blog.likes}
+            <button>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>
+      }
+
+    </div>
+  )
+}
 
 export default Blog

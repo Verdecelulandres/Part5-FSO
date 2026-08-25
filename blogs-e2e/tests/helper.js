5964c854bb703646acde641989dd6b8959b7fa8b
Andres Laverde <andres.laverde@protonmail.com>
2026-08-25 11:22:27 -0700

    create: helper login function

---
+++ blogs-e2e/tests/helper.js
@@ -0,0 +1,7 @@
+const login = async (page, username, password) => {
+  await page.getByLabel('username').fill(username);
+  await page.getByLabel('password').fill(password);
+  await page.getByRole('button', { name: 'login' }).click();
+}
+
+export { login }

488ead0de46f545116e4227b6121516630337dc6
Andres Laverde <andres.laverde@protonmail.com>
2026-08-26 10:22:56 -0700

    update: fix blog unit test to show like btn

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -5,69 +5,68 @@ import { expect } from 'vitest';
 
 describe('<Blog /> tests', () => {
 
-  let blogContainer = null;
+  const blog = {
+    title: 'Test blog',
+    author: 'test',
+    url: 'www.test.com',
+    likes: 1,
+    user: {
+      name: 'testUser',
+      id: '1'
+    }
+  }
   const likeBlog = vi.fn();
 
-  beforeEach(() => {
-    const blog = {
-      title: 'Test blog',
-      author: 'test',
-      url: 'www.test.com',
-      likes: 1,
-      user: {
-        name: 'testUser',
-        id: '1'
-      }
-    }
-
-    const { container } = render(<Blog blog={blog} likeBlog={likeBlog} />);
-    blogContainer = container;
-  });
   test('Renders certain blog parts by default', () => {
-    const titleSpan = blogContainer.querySelector('.blog-title');
-    const authorSpan = blogContainer.querySelector('.blog-author');
-    const urlDiv = blogContainer.querySelector('.blog-url');
+    const { container } = render(<Blog blog={blog} likeBlog={likeBlog} />);
+
+    const titleSpan = container.querySelector('.blog-title');
+    const authorSpan = container.querySelector('.blog-author');
+    const urlDiv = container.querySelector('.blog-url');
+    const userDiv = container.querySelector('.blog-user');
 
     expect(titleSpan).toHaveTextContent('Test blog');
     expect(authorSpan).toHaveTextContent('test:');
     expect(urlDiv).toHaveTextContent('www.test.com');
+    expect(userDiv).toHaveTextContent('Added by testUser');
 
-    const likeSpan = blogContainer.querySelector('.blog-likes');
+    const likeSpan = container.querySelector('.blog-likes');
     expect(likeSpan).toBeVisible();
     expect(likeSpan).toHaveTextContent('likes 1');
 
-    const likeBtn = blogContainer.querySelector('.blog-like-btn');
+    const likeBtn = container.querySelector('.blog-like-btn');
     expect(likeBtn).toBeNull();
 
-    const removeBtn = blogContainer.querySelector('.blog-remove-btn');
+    const removeBtn = container.querySelector('.blog-remove-btn');
     expect(removeBtn).toBeNull();
   });
 
-  test('Show blog hidden data after clicked', async () => {
-    const user = userEvent.setup();
-    const viewBtn = screen.getByText('view');
-    await user.click(viewBtn);
+  describe('When logged in', () => {
 
-    const expandedBlogData = blogContainer.querySelector('.blog_expanded');
+    test('as other user show only like button', async () => {
+      const otherUser = {
+        name: 'otherUser',
+        id: 2
+      }
+      const { container } = render(<Blog blog={blog} likeBlog={likeBlog} loggedUser={otherUser} />);
 
-    expect(expandedBlogData).toBeVisible();
+      const likeBtn = container.querySelector('.blog-like-btn');
+      expect(likeBtn).toBeVisible();
 
-    const urlDiv = blogContainer.querySelector('.blog_url');
-    const likesDiv = blogContainer.querySelector('.blog_likes');
+      const removeBtn = container.querySelector('.blog-remove-btn');
+      expect(removeBtn).toBeNull();
+    });
 
-    expect(urlDiv).toHaveTextContent('www.test.com');
-    expect(likesDiv).toHaveTextContent('1');
-  });
+    test('Ensure like callback is called correctly', async () => {
+      const user = userEvent.setup();
+      const viewBtn = screen.getByText('view');
+      await user.click(viewBtn);
 
-  test('Ensure like callback is called correctly', async () => {
-    const user = userEvent.setup();
-    const viewBtn = screen.getByText('view');
-    await user.click(viewBtn);
+      const likeBtn = screen.getByText('like');
+      await user.click(likeBtn);
+      await user.click(likeBtn);
 
-    const likeBtn = screen.getByText('like');
-    await user.click(likeBtn);
-    await user.click(likeBtn);
-
-    expect(likeBlog.mock.calls).toHaveLength(2);
+      expect(likeBlog.mock.calls).toHaveLength(2);
+    });
   });
 });

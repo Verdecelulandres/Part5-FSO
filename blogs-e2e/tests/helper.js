b7311229bdad1e57eea1bb9a29fb6e83b916bc2b
Andres Laverde <andres.laverde@protonmail.com>
2026-08-26 12:03:06 -0700

    e2e-fix: wait for login to finish redirect

--- blogs-e2e/tests/helper.js
+++ blogs-e2e/tests/helper.js
@@ -2,10 +2,11 @@ const login = async (page, username, password) => {
   await page.getByLabel('username').fill(username);
   await page.getByLabel('password').fill(password);
   await page.getByRole('button', { name: 'login' }).click();
+  await page.getByRole('heading', { name: 'blogs' }).waitFor();
 }
 
 const createBlog = async (page, title, author, url) => {
-  // await page.getByRole('button', { name: 'create new blog' }).click();
+  await page.goto('/create');
   await page.getByLabel('title:').fill(title);
   await page.getByLabel('author:').fill(author);
   await page.getByLabel('url:').fill(url);
@@ -19,16 +20,16 @@ const getExpandedBlogContainer = async (page, blogToFind) => {
   return blogDiv;
 }
 
-const likeBlog = async (blogToLike) => {
-  let likeSpanContent = await blogToLike
+const likeBlog = async (page) => {
+  let likeSpanContent = await page
     .getByText(/^likes \d$/)
     .textContent();
   likeSpanContent = likeSpanContent.split(' ');
   likeSpanContent[1] = String(Number(likeSpanContent[1]) + 1);
   likeSpanContent = likeSpanContent.join(' ');
 
-  await blogToLike.getByRole('button', { name: 'like' }).click();
-  await blogToLike.getByText(likeSpanContent, { exact: true }).waitFor();
+  await page.getByRole('button', { name: 'like' }).click();
+  await page.getByText(likeSpanContent, { exact: true }).waitFor();
 }
 
 export { login, createBlog, getExpandedBlogContainer, likeBlog }

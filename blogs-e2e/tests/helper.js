d666943cfced54b3916e816130cafd89b3be78f8
Andres Laverde <andres.laverde@protonmail.com>
2026-08-26 12:17:30 -0700

    e2e-fix: include navigation in login helper

--- blogs-e2e/tests/helper.js
+++ blogs-e2e/tests/helper.js
@@ -1,4 +1,6 @@
 const login = async (page, username, password) => {
+  await page.goto('/login');
+  await page.getByRole('heading', { name: 'Login to application' }).waitFor();
   await page.getByLabel('username').fill(username);
   await page.getByLabel('password').fill(password);
   await page.getByRole('button', { name: 'login' }).click();
